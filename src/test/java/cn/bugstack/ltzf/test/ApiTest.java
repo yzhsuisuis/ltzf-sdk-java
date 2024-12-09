3e7a1b0ad68f4f2cc6a4be9c15d8a7f6576ebf15
yangbo <972849883@qq.com>
2024-12-09 23:43:23 +0800

    project init

---
+++ src/test/java/cn/bugstack/ltzf/test/ApiTest.java
@@ -0,0 +1,13 @@
+package cn.bugstack.ltzf.test;
+
+/*
+ *@auther:yangzihe @洋纸盒
+ *@discription:
+ *@create 2024-12-09 23:37
+ */
+
+public class ApiTest {
+    public static void main(String[] args) {
+        System.out.println("yangbo");
+    }
+}
